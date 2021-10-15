6865c86f958141e0abe468f67e5a40b6ac663aa8
Joana Brito <joanafiorentini@hotmail.com>
2021-10-15 15:34:24 -0700

    Import components to app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { Route, Switch } from "react-router";
+import Landing from "/Landing/Landing";
+import Header from "/Header/Header";
+import Footer from "/Footer/Footer";
 import './App.css';
 
 const App = () => {
@@ -13,15 +16,38 @@ const App = () => {
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/"/>
-        <Route exact path="/all-categories"/>
-        <Route exact path="/recipies-by-category"/>
-        <Route exact path="/:id"/>
+        <Route exact path="/" render={ () => { 
+            <section className='landing-page'>
+              <Landing />
+            </section>
+          }
+        }/>
+        <Route exact path="/all-categories" render={ () => {
+            <section className='categories-page'>
+              <Header/>
+              <Footer/>
+            </section>
+          }
+        }/>
+        <Route exact path="/recipies-by-category" render={() => {
+            <section className='recipies-by-category'>
+              <Header/>
+              <Footer/>
+            </section>
+          }
+        }/>
+        <Route exact path="/:id" render={()=> {
+            <section className='recipie-details'>
+              <Header/>
+              <Footer/>
+            </section>
+          }
+        }/>
       </Switch>
 
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Landing
         </p>
         <a
           className="App-link"
