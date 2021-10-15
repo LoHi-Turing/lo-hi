52d759d3661bcc87f261c5f967c20786ba399193
Joana Brito <joanafiorentini@hotmail.com>
2021-10-15 15:10:45 -0700

    Import hooks and add routers

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,24 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { Route, Switch } from "react-router";
 import './App.css';
 
-function App() {
+const App = () => {
+  
+  
+  
+  
+  
+  
   return (
     <div className="App">
+      <Switch>
+        <Route exact path="/"/>
+        <Route exact path="/all-categories"/>
+        <Route exact path="/recipies-by-category"/>
+        <Route exact path="/:id"/>
+      </Switch>
+
       <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
