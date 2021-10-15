afa4dd318c312e03b5b4b3aac6537448c27dd74e
Joana Brito <joanafiorentini@hotmail.com>
2021-10-15 16:21:09 -0700

    Start landing page

---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Landing.css';
+
+
+const Landing = () => {
+    return (
+        <section className='landing'>
+            Heloooooo
+        </section>
+    )
+}
+
+export default Landing;
