75883a0d47dbfe97eef09472cf32fa9a5e0b02ea
Megan McBride <megan.mcbride@colorado.edu>
2021-10-15 21:27:48 -0600

    Add Link to the first category and different recipes

--- src/components/AllCategories/AllCategories.js
+++ src/components/AllCategories/AllCategories.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import './AllCategories.css'
+import './AllCategories.css';
+import { Link } from 'react-router-dom';
 
 const AllCategories = () => {
-
+  return (
+    <section>
+      <h1>Baking Categories</h1>
+      <Link>Cakes</Link>
+      <Link>Bread</Link>
+      <Link>Cookies</Link>
+      <Link>Muffins</Link>
+      <Link>Pancakes</Link>
+    </section> 
+  )
 }
 
 export default AllCategories;
