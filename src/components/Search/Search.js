4bfd137413409cd8a247f0c2291b0e097f5accbe
Megan McBride <megan.mcbride@colorado.edu>
2021-10-16 15:53:40 -0600

    Commenting out a function to handle the Enter key use and Troubleshooting api calls

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { formatLocation } from '../../utils/utilFunctions';
 import './Search.css';
 
 const Search = ({ updateLocation }) => {
@@ -7,7 +8,8 @@ const Search = ({ updateLocation }) => {
     const [location, setLocation] = useState('')
 
     const handleClick = () => {
-       updateLocation(location)
+        // const updatedLocation = formatLocation(location)
+        updateLocation(location)
     }
 
     // const handleKeyPress = e => {
@@ -30,7 +32,8 @@ const Search = ({ updateLocation }) => {
                 name='location'
                 value={location}
                 onChange={e => setLocation(e.target.value)}
-                // onKeyPress={e => handleKeyPress(e)}/>
+                // onKeyPress={e => handleKeyPress(e)}
+                />
             <Link to='/all-categories'>            
                 <img className='mag-glass' src='https://cdn2.vectorstock.com/i/1000x1000/33/01/magnifying-glass-orange-round-search-icon-vector-20353301.jpg' alt='Search Magnifying Glass' onClick={() => handleClick()}/>
             </Link>
