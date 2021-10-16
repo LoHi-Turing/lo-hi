26bbc64670143959549f246688f1677c4717c7e5
Megan McBride <megan.mcbride@colorado.edu>
2021-10-15 21:46:46 -0600

    Pass props

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -4,14 +4,14 @@ import './Landing.css';
 import brandLogo from '../../images/LoHi.png';
 
 
-const Landing = () => {
+const Landing = ({ updateLocation }) => {
     return (
         <section className='landing'>
             <img className='background-img'src='https://thumbs.dreamstime.com/b/ocher-apron-various-baking-ingredients-eggs-sugar-cinnamon-sticks-kitchen-utensils-white-background-top-view-copy-175923956.jpg' alt='baking'/>
                 <div>
                     <img className='logo' src={brandLogo} alt="LoHi logo"/>
                     <h1>Where your AlTituDe matters!</h1>
-                    <Search/>
+                    <Search updateLocation={updateLocation}/>
                     <h2>How To Use LoHi</h2>
                     <p>With this website you will be able to have recipies adjusted accordently to your altitude </p>
                     <p>The ingredientes that we will adjust will be: Flower , Sugar and Yeast </p>
