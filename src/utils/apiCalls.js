414b4b9e07bee74db74b89c63367999fee8b6373
Megan McBride <megan.mcbride@colorado.edu>
2021-10-16 15:43:03 -0600

    Create API fetch for location

---
+++ src/utils/apiCalls.js
@@ -0,0 +1,4 @@
+export const getLocationData = (currentLocation) => {
+  return fetch(`https://lohi-api.herokuapp.com/api/v1/location?location=${currentLocation}`)
+    .then(res => res.ok ? res : console.log(res))
+}
