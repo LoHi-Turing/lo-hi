bd96b1da571821c3bbcd9a9eaff7f3d0f3ece914
Joana Brito <joanafiorentini@hotmail.com>
2021-10-16 22:05:42 -0700

    Add img element to categories

--- src/components/AllCategories/AllCategories.js
+++ src/components/AllCategories/AllCategories.js
@@ -7,10 +7,15 @@ const AllCategories = () => {
     <main className='categories-display'>
       <h1 className='baking-categories-title'>Baking Categories</h1>
       <section className='links-container'>
+        <img className='cakes-category-img' src='' alt='Cakes category'/>
         <Link className='links'>Cakes</Link>
+        <img className='bread-category-img' src='' alt='Bread category'/>
         <Link className='links'>Bread</Link>
+        <img className='cookies-category-img' src='' alt='Cookies category'/>
         <Link className='links'>Cookies</Link>
+        <img className='muffins-category-img' src='' alt='Muffin category'/>
         <Link className='links'>Muffins</Link>
+        <img className='pancakes-category-img' src='' alt='Pancakes category'/>
         <Link className='links'>Pancakes</Link>
       </section>
     </main> 
