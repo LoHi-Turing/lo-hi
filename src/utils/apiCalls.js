57f747c171ffc470fc06d91401063c245a4dc5ab
Megan McBride <megan.mcbride@colorado.edu>
2021-10-16 19:12:04 -0600

    Trying to troubleshoot API calls

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,5 +1,5 @@
 export const getLocationData = (currentLocation) => {
-  console.log(`https://lohi-api.herokuapp.com/api/v1/location?location=${currentLocation}`)
   return fetch(`https://lohi-api.herokuapp.com/api/v1/location?location=${currentLocation}`)
-    .then(res => res.ok ? res : console.log(res))
+    // .then(res => res.ok ? res : console.log(res))
+    .then(res => console.log(res))
 }
