95a548d3226909f11c1d96327e4fb152052cecbd
Megan McBride <megan.mcbride@colorado.edu>
2021-10-18 20:52:24 -0600

    Removing unnecessary event prevent default and commented out code

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { formatLocation } from '../../utils/utilFunctions';
 import './Search.css';
 
 const Search = ({ updateLocation }) => {
@@ -8,7 +7,6 @@ const Search = ({ updateLocation }) => {
     const [location, setLocation] = useState('')
 
     const handleClick = () => {
-        // const updatedLocation = formatLocation(location)
         updateLocation(location)
     }
 
@@ -21,8 +19,6 @@ const Search = ({ updateLocation }) => {
 
     return (
         <form className='search-field'>
-        {/* I've started the idea of having the onChange function, did not went to far so you we can decide together if you prefer */}
-            {/* <input type='search' className='search-bar' placeholder='ZipCode or City Name' onChange={(e) => handleChange(e)}/> */}
             <input 
                 type='search' 
                 className='search-bar' 
@@ -30,7 +26,6 @@ const Search = ({ updateLocation }) => {
                 name='location'
                 value={location}
                 onChange={e => setLocation(e.target.value)}
-                // onKeyPress={e => handleKeyPress(e)}
                 />
             <Link to='/all-categories' onKeyPress={e => handleKeyPress(e)}>            
                 <img className='mag-glass' src='https://cdn2.vectorstock.com/i/1000x1000/33/01/magnifying-glass-orange-round-search-icon-vector-20353301.jpg' alt='Search Magnifying Glass' onClick={() => handleClick()}
