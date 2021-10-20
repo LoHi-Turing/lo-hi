03d70119689c46df74987b646f55e8222f6f2b6b
Joana Brito <joanafiorentini@hotmail.com>
2021-10-19 19:14:55 -0700

    Change class names to be more cohesive and add props

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './RecipeCard.css';
 
 
-const RecipeCard = () => {
+const RecipeCard = (props) => {
 
     return (
-        <Link to= { `/${ category }/${ recipie.id }`}>
-            <section className='recipe-links-container'>
-                <div className='recipe-main-img-link'>
-                    <img className='recipe-main-img' src='https://cdn.shopify.com/s/files/1/0258/8531/products/Lemon_Bundt_Cake_grande.jpg?v=1564603405' alt='{ Recipe.title }'/>
-                    {/* <Link className='links'>'{ Recipe.title }'</Link> */}
+        <Link to= { `/${ props.category }/${ props.id }`}>
+            <section className='recipe-card-container'>
+                <div className='recipe-card-main-img-container'>
+                    <img className='recipe-card-main-img' src='https://cdn.shopify.com/s/files/1/0258/8531/products/Lemon_Bundt_Cake_grande.jpg?v=1564603405' alt='{ props.title }'/>
+                    {/* <Link className='links'>'{ props.title }'</Link> */}
                 </div>
             </section>
         </Link>
