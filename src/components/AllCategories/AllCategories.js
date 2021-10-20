167bff4ecff6ff36d07df26634676c5cf20a14a1
Joana Brito <joanafiorentini@hotmail.com>
2021-10-19 21:45:33 -0700

    Correct Syntax names on the import of the sample data and link to upercase to match the data
    Correct file name file-import sample data

--- src/components/AllCategories/AllCategories.js
+++ src/components/AllCategories/AllCategories.js
@@ -9,23 +9,23 @@ const AllCategories = () => {
       <section className='links-container'>
         <div className='categories-img-link'>
           <img className='category-img' src='https://cdn.shopify.com/s/files/1/0258/8531/products/Lemon_Bundt_Cake_grande.jpg?v=1564603405' alt='Cakes category'/>
-          <Link to='/cake' className='links'>Cake</Link>
+          <Link to='/Cake' className='links'>Cake</Link>
         </div>
         <div className='categories-img-link'>
           <img className='category-img' src='https://cdn.shopify.com/s/files/1/0258/8531/products/Lemon_Bundt_Cake_grande.jpg?v=1564603405' alt='Bread category'/>
-          <Link to='/bread' className='links'>Bread</Link>
+          <Link to='/Bread' className='links'>Bread</Link>
         </div>
         <div className='categories-img-link'>
           <img className='category-img' src='https://cdn.shopify.com/s/files/1/0258/8531/products/Lemon_Bundt_Cake_grande.jpg?v=1564603405' alt='Cookies category'/>
-          <Link to='/cookie'className='links'>Cookie</Link>
+          <Link to='/Cookie'className='links'>Cookie</Link>
         </div>
         <div className='categories-img-link'>
           <img className='category-img' src='https://cdn.shopify.com/s/files/1/0258/8531/products/Lemon_Bundt_Cake_grande.jpg?v=1564603405' alt='Muffin category'/>
-          <Link to='/muffin' className='links'>Muffin</Link>
+          <Link to='/Muffin' className='links'>Muffin</Link>
         </div>
         <div className='categories-img-link'>
           <img className='category-img' src='https://cdn.shopify.com/s/files/1/0258/8531/products/Lemon_Bundt_Cake_grande.jpg?v=1564603405' alt='Pancakes category'/>
-          <Link to='/pancakes' className='links'>Pancakes</Link>
+          <Link to='/Pancakes' className='links'>Pancakes</Link>
         </div>
       </section>
     </main> 
