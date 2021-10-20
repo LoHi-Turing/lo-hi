9969c4d33209d7ea2209eaf88e2478e20a1b827f
Joana Brito <joanafiorentini@hotmail.com>
2021-10-19 23:35:45 -0700

    Add image element for the icons to be shown in the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -28,7 +28,9 @@ const Header = ({ location, elevation, humidity, updateLocation}) => {
       </div> 
       <div className='location-info'>
         <div className='location-data'>
+          <img className='elevation-icon' src='' alt='Elevation Icon'/>
           <p className='p-elevation'>{elevation} m<sup>2</sup></p>
+          <img className='humidity-icon' src='' alt='Humidity Icon'/>
           <p className='p-humidity'>{humidity}%</p>
         </div>
         <h1 className='company-name'>LoHi</h1>
