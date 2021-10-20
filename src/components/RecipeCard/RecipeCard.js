dbb4ef2a644155d5c7a0e2c7fdd2ed89cf726c1e
Joana Brito <joanafiorentini@hotmail.com>
2021-10-19 19:11:09 -0700

    Add Link Router to the card

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -5,12 +5,14 @@ import './RecipeCard.css';
 const RecipeCard = () => {
 
     return (
-        <section className='recipe-links-container'>
-        <div className='recipe-main-img-link'>
-          <img className='recipe-main-img' src='https://cdn.shopify.com/s/files/1/0258/8531/products/Lemon_Bundt_Cake_grande.jpg?v=1564603405' alt='{ Recipe.title }'/>
-          <Link className='links'>'{ Recipe.title }'</Link>
-        </div>
-      </section>
+        <Link to= { `/${ category }/${ recipie.id }`}>
+            <section className='recipe-links-container'>
+                <div className='recipe-main-img-link'>
+                    <img className='recipe-main-img' src='https://cdn.shopify.com/s/files/1/0258/8531/products/Lemon_Bundt_Cake_grande.jpg?v=1564603405' alt='{ Recipe.title }'/>
+                    {/* <Link className='links'>'{ Recipe.title }'</Link> */}
+                </div>
+            </section>
+        </Link>
     )
 }
 
