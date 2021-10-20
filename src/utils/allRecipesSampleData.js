9af1445a2af41ac1f13e665f2cd5754a7151c1cc
Joana Brito <joanafiorentini@hotmail.com>
2021-10-19 20:18:33 -0700

    Change sample data file to more accurate one

--- src/utils/allRecipesSampleData.js
+++ src/utils/allRecipesSampleData.js
@@ -1,74 +1,48 @@
-export  const allRecipeSampleData = () => {
+export  const allRecipeSampleData = {
     "data": [
-       {
-        "id": 1,
-        "type": "recipe",
-        "attributes": {
-            "category": "Cookie",
-            "title": "Chocolate Chip Cookies",
-            "ingredients": [
-                { flour: 100g }, 
-                { sugar: 15g },
-                { chocolate_chip: 25g }
-             ],
-            "description": [
-                step 1, 
-                step 2, 
-                step 3
-             ],
-            "cook_time": "1hr 10m", 
-            "images": [
-                "image 1", 
-                "image 2", 
-                "image 3" 
-            ]
-        }, 
         {
-        "id": 2,
-        "type": "recipe",
-        "attributes": {
-            "category": "Cookie",
-            "title": "Chocolate Chip Cookies",
-            "ingredients": [
-                { flour: 100g }, 
-                { sugar: 15g },
-                { chocolate_chip: 25g }
-             ],
-            "description": [
-                step 1, 
-                step 2, 
-                step 3
-             ],
-            "cook_time": "1hr 10m",
-            "images": [
-                "image 1", 
-                "image 2", 
-                "image 3" 
-            ]
-        }, 
+            "id": "6",
+            "type": "recipe",
+            "attributes": {
+                "title": "Chocolate Chip Banana Bread",
+                "description": "[\"Preheat the oven to 350ºF. Prepare a 9x5 baking pan by lining it with parchment or foil and lightly spraying with nonstick spray.\", \"Preheat the oven to 350ºF. Prepare a 9x5 loaf baking pan by lining it with parchment or foil and lightly spraying nonstick spray.\", \"Mash the bananas with a fork or whisk. Whisk in the sugar, egg, vanilla, and vegetable oil.\", \"Mix in the flour, baking powder, baking soda, cinnamon, and salt. The batter will be lumpy.\", \"Fold in the chocolate chips.\", \"Pour the batter into the prepared baking pan (top with more chocolate chips if you want). Bake for 55-60 minutes (start checking for doneness with a toothpick at 55 minutes). Let cool.\"]",
+                "category": "Bread",
+                "cook_time": "1 HOUR 5 MINUTES",
+                "ingredients": [
+                    "Overripe Bananas: 3-4 medium",
+                    "Sugar: .75 cups",
+                    "Egg: 1 large",
+                    "Vanilla: 1 teaspoons",
+                    "Vegetable Oil: .25 cups",
+                    "All Purpose Flour: 1.5 cups",
+                    "Baking Powder: 1.25 teaspoons",
+                    "Baking Soda: .5 teaspoons",
+                    "Cinnamon: 1 dash",
+                    "Salt: .125 teaspoons",
+                    "Chocolate Chips: 1 cups"
+                ]
+            }
+        },
         {
-        "id": 3,
-        "type": "recipe",
-        "attributes": {
-            "category": "Cookie",
-            "title": "Chocolate Chip Cookies",
-            "ingredients": [
-                { flour: 100g }, 
-                { sugar: 15g },
-                { chocolate_chip: 25g }
-             ],
-            "description": [
-                step 1, 
-                step 2, 
-                step 3
-             ],
-            "cook_time": "1hr 10m",
-            "images": [
-                "image 1", 
-                "image 2", 
-                "image 3" 
-            ]
+            "id": "7",
+            "type": "recipe",
+            "attributes": {
+                "title": "Fluffy Fluffy Pancakes",
+                "description": "[\"Combine flour, sugar, salt, and baking powder into a bowl and mix well.\", \"In a separate bowl, mix the egg, milk, vanilla, and melted butter together. Be careful not to use hot, melted butter, as that may end up cooking your eggs!\", \"Pour the wet ingredient mixture into the dry ingredients and stir till mostly incorporated. The batter should be thick and a little lumpy, but without any visible streaks of flour. If it is too thick to work with, add a little milk. Set the batter aside for 5-10 minutes.\", \"Heat a heavy-bottomed, non-stick pan over a low-medium heat and coat it with oil. Use a ¼ cup measure to scoop and drop the batter into the pan so that you get evenly-sized pancakes.\", \"Cook until the first side is golden brown, or until the top surface forms bubbles. Flip and repeat on the other side. Adjust heat accordingly. Serve while still hot.\"]",
+                "category": "Pancakes",
+                "cook_time": "15 MINUTES",
+                "ingredients": [
+                    "All-Purpose Flour: 1 cups",
+                    "Baking Powder: 2 teaspoons",
+                    "Salt: 1 pinch",
+                    "White Sugar: 2 tablespoons",
+                    "Milk: .75 cups",
+                    "Milk: 2 teaspoons",
+                    "Egg: 1 Large",
+                    "Unsalted Butter: 2 tablespoons",
+                    "Pure Vanilla Extract: 1 teaspoons"
+                ]
+            }
         }
-      ]
-    }
+    ]
 }
