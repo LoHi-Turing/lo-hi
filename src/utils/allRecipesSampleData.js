d2a63ac8779c97e63e7e15d025b3897376d54c62
Joana Brito <joanafiorentini@hotmail.com>
2021-10-19 20:13:46 -0700

    Create new file with sample data

---
+++ src/utils/allRecipesSampleData.js
@@ -0,0 +1,74 @@
+export  const allRecipeSampleData = () => {
+    "data": [
+       {
+        "id": 1,
+        "type": "recipe",
+        "attributes": {
+            "category": "Cookie",
+            "title": "Chocolate Chip Cookies",
+            "ingredients": [
+                { flour: 100g }, 
+                { sugar: 15g },
+                { chocolate_chip: 25g }
+             ],
+            "description": [
+                step 1, 
+                step 2, 
+                step 3
+             ],
+            "cook_time": "1hr 10m", 
+            "images": [
+                "image 1", 
+                "image 2", 
+                "image 3" 
+            ]
+        }, 
+        {
+        "id": 2,
+        "type": "recipe",
+        "attributes": {
+            "category": "Cookie",
+            "title": "Chocolate Chip Cookies",
+            "ingredients": [
+                { flour: 100g }, 
+                { sugar: 15g },
+                { chocolate_chip: 25g }
+             ],
+            "description": [
+                step 1, 
+                step 2, 
+                step 3
+             ],
+            "cook_time": "1hr 10m",
+            "images": [
+                "image 1", 
+                "image 2", 
+                "image 3" 
+            ]
+        }, 
+        {
+        "id": 3,
+        "type": "recipe",
+        "attributes": {
+            "category": "Cookie",
+            "title": "Chocolate Chip Cookies",
+            "ingredients": [
+                { flour: 100g }, 
+                { sugar: 15g },
+                { chocolate_chip: 25g }
+             ],
+            "description": [
+                step 1, 
+                step 2, 
+                step 3
+             ],
+            "cook_time": "1hr 10m",
+            "images": [
+                "image 1", 
+                "image 2", 
+                "image 3" 
+            ]
+        }
+      ]
+    }
+}
