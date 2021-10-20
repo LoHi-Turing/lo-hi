3abf276299a46300ed12c270b7ec5dc730bc9248
Joana Brito <joanafiorentini@hotmail.com>
2021-10-19 19:06:49 -0700

    Start basic component

---
+++ src/components/RecipesByCategory/RecipesByCategory.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './RecipesByCategory.css';
+
+
+const RecipesByCategory = () => {
+
+    // map to recipes and create/ show recipe card
+    return (
+        <main className='recipes-by-category'>
+        <h1 className='category-title'>"Category" Recipes</h1>
+        <RecipeCard />
+        {/* <section className='recipe-links-container'>
+          <div className='recipe-main-img-link'>
+            <img className='recipe-main-img' src='https://cdn.shopify.com/s/files/1/0258/8531/products/Lemon_Bundt_Cake_grande.jpg?v=1564603405' alt='Cakes category'/>
+            <Link className='links'>Amazing Chocolate Cake</Link>
+          </div>
+        </section> */}
+      </main> 
+    )
+}
+
+export default RecipesByCategory;
