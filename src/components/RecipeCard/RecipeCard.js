a72f33482eee4fffc220fa31d0339c29bb1305c6
Joana Brito <joanafiorentini@hotmail.com>
2021-10-19 21:00:08 -0700

    Add the sample data correct info

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './RecipeCard.css';
 
 
-const RecipeCard = (props) => {
+const RecipeCard = ({ id, category, title, /* img */ }) => {
 
     return (
-        <Link to= { `/${ props.category }/${ props.id }`}>
+        <Link to= { `/${ category }/${ id }`}>
             <section className='recipe-card-container'>
                 <div className='recipe-card-main-img-container'>
-                    <img className='recipe-card-main-img' src='https://cdn.shopify.com/s/files/1/0258/8531/products/Lemon_Bundt_Cake_grande.jpg?v=1564603405' alt='{ props.title }'/>
+                    <img className='recipe-card-main-img' src='https://cdn.shopify.com/s/files/1/0258/8531/products/Lemon_Bundt_Cake_grande.jpg?v=1564603405' alt={ title }/>
+                    <h3 className='recipie-card-title'>{ title }</h3>
                     {/* <Link className='links'>'{ props.title }'</Link> */}
                 </div>
             </section>
