e69122d36466192e0db1711e15d757d86fe29915
Megan McBride <megan.mcbride@colorado.edu>
2021-10-20 20:43:25 -0600

    Add Link back to Recipe Card

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -6,7 +6,7 @@ import './RecipeCard.css';
 const RecipeCard = ({ id, category, title, /* img */ }) => {
 
     return (
-        // <Link to= {`/${ category }/${ id }`} >
+        <Link to= {`/${ category }/${ id }`} >
             <section className='recipe-card-container'>
                 <div className='recipe-card-main-img-container'>
                     <img className='recipe-card-main-img' src='https://cdn.shopify.com/s/files/1/0258/8531/products/Lemon_Bundt_Cake_grande.jpg?v=1564603405' alt={ title }/>
@@ -14,7 +14,7 @@ const RecipeCard = ({ id, category, title, /* img */ }) => {
                     {/* <Link className='links'>'{ props.title }'</Link> */}
                 </div>
             </section>
-        ///* </Link> */
+        </Link> 
     )
 }
 
