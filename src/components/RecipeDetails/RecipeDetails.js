5e833174af19d0b2d7111bf621b044d26f30a3cd
Megan McBride <megan.mcbride@colorado.edu>
2021-10-20 21:07:59 -0600

    Rendering and format recipe details

--- src/components/RecipeDetails/RecipeDetails.js
+++ src/components/RecipeDetails/RecipeDetails.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './RecipeDetails.css';
 
-const RecipeDetails = () => {
+const RecipeDetails = ({ categoryType, recipeId, currentRecipe }) => {
+  const ingredientList = currentRecipe.attributes.ingredients.map(ingredient => (
+    <li>{ingredient}</li>
+  ))
   return (
     <section className='recipe-details-container'>
         <div className='recipe-details-main-img-container'>
-        <img className='recipe-details-main-img' src='https://cdn.shopify.com/s/files/1/0258/8531/products/Lemon_Bundt_Cake_grande.jpg?v=1564603405' alt='image'/>
-        <h3 className='recipie-details-title'>Howdy</h3>
+        <img className='recipe-details-main-img' src='https://cdn.shopify.com/s/files/1/0258/8531/products/Lemon_Bundt_Cake_grande.jpg?v=1564603405' alt='baked good'/>
+        <h3 className='recipie-details-title'>{currentRecipe.attributes.title}</h3>     
+        <h3>Ingredients</h3>   
+        <ul>{ingredientList}</ul>
+        <h3>Instruction</h3>
+        <p>{currentRecipe.attributes.description}</p>        
+        <p>{currentRecipe.attributes.cook_time}</p>
       </div>
     </section>
   )
