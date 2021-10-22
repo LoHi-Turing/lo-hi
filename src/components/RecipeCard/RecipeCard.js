701e38d512933cdfa7bd08a69251fb66849b52f4
Joana Brito <joanafiorentini@hotmail.com>
2021-10-22 15:54:18 -0700

    Comment console.log

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -9,7 +9,8 @@ const RecipeCard = ({ id, category, title, /* img */ }) => {
         <Link to= {`/${ category }/${ id }`} >
             <section className='recipe-card-container'>
                 <div className='recipe-card-main-img-container'>
-                    <img className='recipe-card-main-img' src='https://cdn.shopify.com/s/files/1/0258/8531/products/Lemon_Bundt_Cake_grande.jpg?v=1564603405' alt={ title }/>
+                    {/* <img className='recipe-card-main-img' src=`images/${ title }.png` alt={ title }/> */}
+                    <img className='recipe-card-main-img' src='https://cdn.leitesculinaria.com/wp-content/uploads/2016/01/five-minute-artisan-bread-recipe.jpg.webp' alt={ title }/>
                     <h3 className='recipie-card-title'>{ title }</h3>
                     {/* <Link className='links'>'{ props.title }'</Link> */}
                 </div>
