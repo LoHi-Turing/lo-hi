39d24e23e4ef2a3b802348e07260027a3c212d21
Joana Brito <joanafiorentini@hotmail.com>
2021-10-22 16:47:08 -0700

    Add images with draws

--- src/components/AllCategories/AllCategories.js
+++ src/components/AllCategories/AllCategories.js
@@ -10,25 +10,25 @@ const AllCategories = () => {
       <h2 className='baking-categories-title'>Baking Categories</h2>
       <section className='links-container-line1'>
         <div className='categories-img-link'>
-          <img className='category-img' src='https://cdn.shopify.com/s/files/1/0258/8531/products/Lemon_Bundt_Cake_grande.jpg?v=1564603405' alt='Cakes category'/>
+          <img className='category-img' src='https://media.istockphoto.com/vectors/birthday-cake-vector-vector-id1144957667' alt='Cakes category'/>
           <Link to='/Cake' className='links'>Cake</Link>
         </div>
         <div className='categories-img-link'>
-          <img className='category-img' src='https://cdn.shopify.com/s/files/1/0258/8531/products/Lemon_Bundt_Cake_grande.jpg?v=1564603405' alt='Bread category'/>
+          <img className='category-img' src='https://cdn.leitesculinaria.com/wp-content/uploads/2016/01/five-minute-artisan-bread-recipe.jpg.webp' alt='Bread category'/>
           <Link to='/Bread' className='links'>Bread</Link>
         </div>
         <div className='categories-img-link'>
-          <img className='category-img' src='https://cdn.shopify.com/s/files/1/0258/8531/products/Lemon_Bundt_Cake_grande.jpg?v=1564603405' alt='Cookies category'/>
+          <img className='category-img' src='images/muffin.png' alt='Cookies category'/>
           <Link to='/Cookie'className='links'>Cookie</Link>
         </div>
       </section>
       <section className='links-container-line2'>
         <div className='categories-img-link'>
-          <img className='category-img' src='https://cdn.shopify.com/s/files/1/0258/8531/products/Lemon_Bundt_Cake_grande.jpg?v=1564603405' alt='Muffin category'/>
+          <img className='category-img' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZtEVmn0gbLTs9_owYEKIiMK2ueWl9tk9lVvEsCcL46uKlkolaEA4NB_iAqfuH5cXiPrc&usqp=CAU' alt='Muffin category'/>
           <Link to='/Muffin' className='links'>Muffin</Link>
         </div>
         <div className='categories-img-link'>
-          <img className='category-img' src='https://cdn.shopify.com/s/files/1/0258/8531/products/Lemon_Bundt_Cake_grande.jpg?v=1564603405' alt='Pancakes category'/>
+          <img className='category-img' src='https://media.istockphoto.com/vectors/pancakes-with-maple-syrup-outline-illustration-vector-id1312306188' alt='Pancakes category'/>
           <Link to='/Pancakes' className='links'>Pancakes</Link>
         </div>
       </section>
