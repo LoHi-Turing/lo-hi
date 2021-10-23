b4edc1d3192ac53b1fd21a729203d89e9a63c480
Megan McBride <megan.mcbride@colorado.edu>
2021-10-22 21:21:43 -0600

    Add more tests to recipe details page

--- cypress/integration/recipe_details_spec.js
+++ cypress/integration/recipe_details_spec.js
@@ -5,11 +5,11 @@ describe('Recipe Details', () => {
       .type('90210')
       .get('.mag-glass').click()
       .get('.links-container-line1 > :nth-child(2) > .links').click()
-      .get('.recipie-card-title').click()
+      .get('.recipe-card-title').click()
   })
 
   it('Should include the id in the url', () => {
-    cy.url().should('include', '6')
+    cy.url().should('include', '1')
   })
 
   it('Should display the same header', () => {
@@ -29,4 +29,16 @@ describe('Recipe Details', () => {
       .url().should('include', 'Bread')
       .get('.recipes-by-category').should('be.visible')
   })
+
+  it('Should display the recipe title', () => {
+    cy.get('.recipie-details-title').should('contain', 'Chocolate Chip Banana Bread')
+  })
+
+  it('Should display ingredients', () => {
+    cy.get('ul > :nth-child(1)').should('contain', 'Overripe Bananas')
+      .get('ul > :nth-child(6)').should('contain', 'All Purpose Flour')
+      .get('ul > :nth-child(9)').should('contain', 'Cinnamon')
+  })
+
+  
 })
