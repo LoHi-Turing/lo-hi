21f09ff87712121d30f6b4df1ff068a92edfd84e
Joana Brito <joanafiorentini@hotmail.com>
2021-10-22 17:12:52 -0700

    Fix syntax error

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -11,7 +11,7 @@ const RecipeCard = ({ id, category, title, /* img */ }) => {
                 <div className='recipe-card-main-img-container'>
                     {/* <img className='recipe-card-main-img' src=`images/${ title }.png` alt={ title }/> */}
                     <img className='recipe-card-main-img' src='https://cdn.leitesculinaria.com/wp-content/uploads/2016/01/five-minute-artisan-bread-recipe.jpg.webp' alt={ title }/>
-                    <h3 className='recipie-card-title'>{ title }</h3>
+                    <h3 className='recipe-card-title'>{ title }</h3>
                     {/* <Link className='links'>'{ props.title }'</Link> */}
                 </div>
             </section>
