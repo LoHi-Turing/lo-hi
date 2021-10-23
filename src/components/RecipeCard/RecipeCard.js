f499bfebdbe22d23d5982deba232994d445597c8
Joana Brito <joanafiorentini@hotmail.com>
2021-10-23 16:07:15 -0700

    Delete Unnecessary code

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -6,10 +6,10 @@ import './RecipeCard.css';
 const RecipeCard = ({ id, category, title, /* img */ }) => {
 
     return (
-        <Link /*className='link-to-recipe-details'*/ to= {`/${ category }/${ id }`} >
+        <Link className='link-to-recipe-details' to= {`/${ category }/${ id }`} >
             <section className='recipe-card-container'>
                 <div className='recipe-card-main-img-container'>
-                    {/* <img className='recipe-card-main-img' src=`images/${ title }.png` alt={ title }/> */}
+                    {/* <img className='recipe-card-main-img' src=`data info` alt={ title }/> */}
                     <img className='recipe-card-main-img' src='https://cdn.leitesculinaria.com/wp-content/uploads/2016/01/five-minute-artisan-bread-recipe.jpg.webp' alt={ title }/>
                     <h3 className='recipe-card-title'>{ title }</h3>
                     {/* <Link className='links'>'{ props.title }'</Link> */}
