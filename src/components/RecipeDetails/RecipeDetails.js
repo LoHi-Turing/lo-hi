c20dcecfe789c7f7fb54499dc8e309ab54efbb2d
Megan McBride <megan.mcbride@colorado.edu>
2021-10-23 20:35:13 -0600

    Styling Recipe Details

--- src/components/RecipeDetails/RecipeDetails.js
+++ src/components/RecipeDetails/RecipeDetails.js
@@ -10,15 +10,15 @@ const RecipeDetails = ({ categoryType, recipeId, currentRecipe }) => {
 
   return (
     <section className='recipe-details'>
+        <div className='recipe-details-title-cook-time'>
+          <h3 className='recipie-details-title'>{currentRecipe.attributes.title}  </h3> 
+          <p className='recipe-details-cook-time'>Est. Cook Time: {currentRecipe.attributes.cook_time}</p>  
+        </div>        
         <div className='recipe-details-main-container'>
           <div className='recipe-details-images'>
             <img className='recipe-details-main-img text-focus-in' src='https://cdn.shopify.com/s/files/1/0258/8531/products/Lemon_Bundt_Cake_grande.jpg?v=1564603405' alt='baked good'/>
           </div>
-          <div className='recipe-details-information'>
-            <div className='recipe-details-title-cook-time'>
-              <h3 className='recipie-details-title'>{currentRecipe.attributes.title}</h3>   
-              <p className='recipe-details-cook-time'>Est. Cook Time: {currentRecipe.attributes.cook_time}</p>  
-            </div>
+          <div className='recipe-details-information'>            
             <div className='recipe-details-ingredients'>
               <h3 className='recipe-details-ingredient-title'>Ingredients</h3>   
               <div className='ingredient-information'>
