3d04d055e6aed7d149d441e0022d740eca62cf53
Joana Brito <joanafiorentini@hotmail.com>
2021-10-25 01:18:10 -0700

    Create sample data to add carousel

---
+++ src/utils/carouselSampleData.js
@@ -0,0 +1,14 @@
+export const images = [
+    {
+        author:'Joana',
+        url:'https://www.thechunkychef.com/wp-content/uploads/2020/04/Classic-Banana-Bread-cut.jpg'
+    },
+    {
+        author:'Meg',
+        url:'https://img.buzzfeed.com/thumbnailer-prod-us-east-1/55ac7efd43d74a6ead6576b4bfb28d7e/FB_Syphus_BananaBread_v3.jpg'
+    },
+    {
+        author:'Jaccob',
+        url:'https://static.onecms.io/wp-content/uploads/sites/43/2020/04/17/171761.jpg'
+    }
+];
