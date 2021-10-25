4acfedceb55305813670ebdccb03381966ae3ce4
Megan McBride <megan.mcbride@colorado.edu>
2021-10-25 12:42:30 -0600

    Testing elevation

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -6,6 +6,7 @@ export const getLocationData = async(queryLocation) => {
 }
 
 export const getRecipeData = async(elevation) => {
+  console.log(elevation)
   let url = `https://lohi-api.herokuapp.com/api/v1/recipes?elevation=${elevation}`;
   const res = await fetch(url)
   return res
