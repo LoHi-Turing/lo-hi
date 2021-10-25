36b883f57ceb6e321651f0b02f4b741a43e0a037
Megan McBride <megan.mcbride@colorado.edu>
2021-10-25 16:17:18 -0600

    Removed the disabled attribute

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -21,7 +21,7 @@ const Search = ({ updateLocation }) => {
                 onChange={e => setLocation(e.target.value)}
                 />
             {location && <Link to='/all-categories'>            
-                <button className='search-btn' disabled={!location} type='submit' onClick={() => handleClick()}><img className='mag-glass' src='https://cdn2.vectorstock.com/i/1000x1000/33/01/magnifying-glass-orange-round-search-icon-vector-20353301.jpg' alt='Search Magnifying Glass'               
+                <button className='search-btn' type='submit' onClick={() => handleClick()}><img className='mag-glass' src='https://cdn2.vectorstock.com/i/1000x1000/33/01/magnifying-glass-orange-round-search-icon-vector-20353301.jpg' alt='Search Magnifying Glass'               
                 /></button>
             </Link>}
         </form>
