d29d1dbc0ad3fd66f28c44337687e82dd41f6311
Megan McBride <megan.mcbride@colorado.edu>
2021-10-24 23:05:41 -0600

    Add Loading information

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Loading.css';
+
+const Loading = () => {
+  return (
+    <secion className='loading-container'>
+      <h1>Loading...</h1>
+      <img src='images/LoHi.gif' alt='bowl stirring animation'/>
+    </secion>
+  )
+}
+
+export default Loading;
