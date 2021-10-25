e78fbb6b364308827658aef0142b6e31ee13e325
Joana Brito <joanafiorentini@hotmail.com>
2021-10-25 02:02:24 -0700

    Add Carousel component

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import './Carousel.css';
 import { images } from '../../utils/carouselSampleData';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 
 const Carousel = () => {
 
-    const [currImg, setCurrentImg]= useState(0);
+    const [currImg, setCurrImg]= useState(0);
 
     return (
         <section className='carousel'>
@@ -13,9 +15,25 @@ const Carousel = () => {
                 style={{backgroundImage:`url(${images[currImg].url})`}}
             
             >
-                <div className='left'></div>
-                <div className='center'></div>
-                <div className='right'></div>
+                <div 
+                    className='left'
+                    onClick={() => {
+                        currImg > 0 && setCurrImg(currImg - 1);
+                    }}
+                    >
+                    <ArrowBackIosIcon style={{ fontSize: 30 }}/>
+                </div>
+                <div className='center'>
+                    <p className='carousel-p'>Photo by {images[currImg].author}</p>
+                </div>
+                <div 
+                    className='right'
+                    onClick={() => {
+                        currImg < images.length - 1 && setCurrImg(currImg + 1);
+                    }}
+                >
+                    <ArrowForwardIosIcon style={{ fontSize: 30 }}/>
+                </div>
 
             </div>
         </section>
