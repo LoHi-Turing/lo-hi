3961c7772299fc9dcaedbe4a84381fe8d82d832b
Megan McBride <megan.mcbride@colorado.edu>
2021-10-25 12:42:23 -0600

    Fixing late night spelling error

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -3,10 +3,10 @@ import './Loading.css';
 
 const Loading = () => {
   return (
-    <secion className='loading-container'>
+    <section className='loading-container'>
       <h1 className='loading-message' >Loading...</h1>
       <img className='loading-gif' src='images/LoHi.gif' alt='bowl stirring animation'/>
-    </secion>
+    </section>
   )
 }
 
