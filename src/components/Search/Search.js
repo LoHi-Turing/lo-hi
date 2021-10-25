18d40a9b0051cc9ec40043e9a32df4a83ac3efb4
Megan McBride <megan.mcbride@colorado.edu>
2021-10-25 16:37:29 -0600

    Update variable name to be more specific for search

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -4,10 +4,10 @@ import './Search.css';
 
 const Search = ({ updateLocation }) => {
 
-    const [location, setLocation] = useState('')
+    const [userInput, setUserInput] = useState('')
 
     const handleClick = () => {
-        updateLocation(location)
+        updateLocation(userInput)
     }
 
     return (
@@ -16,11 +16,11 @@ const Search = ({ updateLocation }) => {
                 type='search' 
                 className='search-bar' 
                 placeholder='ZipCode or City Name'
-                name='location'
-                value={location}
-                onChange={e => setLocation(e.target.value)}
+                name='userInput'
+                value={userInput}
+                onChange={e => setUserInput(e.target.value)}
                 />
-            {location && <Link to='/all-categories'>            
+            {userInput && <Link to='/all-categories'>            
                 <button className='search-btn' type='submit' onClick={() => handleClick()}><img className='mag-glass' src='https://cdn2.vectorstock.com/i/1000x1000/33/01/magnifying-glass-orange-round-search-icon-vector-20353301.jpg' alt='Search Magnifying Glass'               
                 /></button>
             </Link>}
