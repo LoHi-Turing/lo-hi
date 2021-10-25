f83ea713aff606a333562a309c21b2ce7276a0cd
Megan McBride <megan.mcbride@colorado.edu>
2021-10-25 16:28:44 -0600

    Comment out console  logs

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,12 +1,12 @@
 export const getLocationData = async(queryLocation) => {
   let url = `https://lohi-api.herokuapp.com/api/v1/location?location=${queryLocation}`
-  console.log(url)
+  // console.log(url)
   const res = await fetch(url)
   return res 
 }
 
 export const getRecipeData = async(elevation) => {
-  console.log(elevation)
+  // console.log(elevation)
   let url = `https://lohi-api.herokuapp.com/api/v1/recipes?elevation=${elevation}`;
   const res = await fetch(url)
   return res
