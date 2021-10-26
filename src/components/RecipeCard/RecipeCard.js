d9647a035aeaf2f15a08c33e26370fff26df5850
Joana Brito <joanafiorentini@hotmail.com>
2021-10-26 11:41:07 -0700

    Delete unnecerrary code

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -10,9 +10,7 @@ const RecipeCard = ({ id, category, title, elevation,  img , findSelectedRecipe 
             <section className='recipe-card-container'>
                 <div className='recipe-card-main-img-container'>
                     <img className='recipe-card-main-img' src={ img } alt={ title }/>
-                    {/* <img className='recipe-card-main-img' src='https://cdn.leitesculinaria.com/wp-content/uploads/2016/01/five-minute-artisan-bread-recipe.jpg.webp' alt={ title }/> */}
                     <h3 className='recipe-card-title'>{ title }</h3>
-                    {/* <Link className='links'>'{ props.title }'</Link> */}
                 </div>
             </section>
         </Link> 
