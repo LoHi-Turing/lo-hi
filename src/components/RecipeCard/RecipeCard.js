379b03578a9d0bc69cef9b703ca2eefec5f53480
Joana Brito <joanafiorentini@hotmail.com>
2021-10-26 11:02:39 -0700

    Add logic for image to be shown on the card

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 import './RecipeCard.css';
 
 
-const RecipeCard = ({ id, category, title, elevation, /* img */ findSelectedRecipe }) => {
+const RecipeCard = ({ id, category, title, elevation,  img , findSelectedRecipe }) => {
 
     return (
         <Link className='link-to-recipe-details' to= {`/${ category }/${ elevation }/${ id }`} onClick={() => {findSelectedRecipe(id)}}>
             <section className='recipe-card-container'>
                 <div className='recipe-card-main-img-container'>
-                    {/* <img className='recipe-card-main-img' src=`data info` alt={ title }/> */}
-                    <img className='recipe-card-main-img' src='https://cdn.leitesculinaria.com/wp-content/uploads/2016/01/five-minute-artisan-bread-recipe.jpg.webp' alt={ title }/>
+                    <img className='recipe-card-main-img' src={ img } alt={ title }/>
+                    {/* <img className='recipe-card-main-img' src='https://cdn.leitesculinaria.com/wp-content/uploads/2016/01/five-minute-artisan-bread-recipe.jpg.webp' alt={ title }/> */}
                     <h3 className='recipe-card-title'>{ title }</h3>
                     {/* <Link className='links'>'{ props.title }'</Link> */}
                 </div>
