5ff3c2cd9a54bc4fd93b90e1c8e7d5412a600a23
Megan McBride <megan.mcbride@colorado.edu>
2021-10-27 11:16:06 -0600

    Small adjustment to landing page

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -10,7 +10,7 @@ const Landing = ({ updateLocation }) => {
                     <img className='logo' src='images/LoHi-yellow.png' alt='LoHi logo'/>
                     <h1 className='catch-phrase'>Where your AlTituDe matters!</h1>
                     <Search updateLocation={updateLocation}/>
-                    <h2 className='how-to-use'>How To Use LoHi</h2>
+                    <h2 className='how-to-use'>How To Use LoHi:</h2>
                     <p>We adjust recipies based on your altitude!</p>
                     <p>Our pre-loaded recipies will automatically adjust the most common ingredients changed with altitude: sugar, flour, baking powder and soda. We also provide your local humidity to take into account for possible adjustments in liquids. Other adjustments to consider for high altitude: increased oven temperature and shorter cook time.</p>
                 </section>
