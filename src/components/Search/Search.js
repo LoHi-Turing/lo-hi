df048794fccc1a86ecebbfebd7066274085cc34b
Megan McBride <megan.mcbride@colorado.edu>
2021-10-27 11:10:53 -0600

    Shift format on Search

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -19,7 +19,7 @@ const Search = ({ updateLocation }) => {
                 name='userInput'
                 value={userInput}
                 onChange={e => setUserInput(e.target.value)}
-                />
+            />
             {userInput && <Link to='/all-categories'>            
                 <button className='search-btn' type='submit' onClick={() => handleClick()}><img className='mag-glass' src='https://cdn2.vectorstock.com/i/1000x1000/33/01/magnifying-glass-orange-round-search-icon-vector-20353301.jpg' alt='Search Magnifying Glass'               
                 /></button>
