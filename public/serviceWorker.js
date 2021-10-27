8f3765eb20628b12bb1b4ad7b9f64068698fa401
Megan McBride <megan.mcbride@colorado.edu>
2021-10-26 23:08:49 -0600

    Add caching array of files

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,7 +1,23 @@
 const self = this;
 
+const cacheName = 'v1';
+const cachedAssets = [
+  'index.html', 
+  '/public/images/Lohi-computer-1.png'
+]
+
 self.addEventListener('install', e => {
-  console.log('Service Worker: Installed')
+  console.log('Service Worker: Installed');
+  
+  e.waitUntil(
+    caches
+      .open(cacheName)
+      .then(cache => {
+        console.log('Service Worker: Caching Files')
+        cache.addAll(cachedAssets);
+      })
+      .then(() => self.skipWaiting())
+  );
 })
 
 self.addEventListener('activate', e => {
