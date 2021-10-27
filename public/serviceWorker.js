f2da270424b2320a4b529cb5c9bcd8a25f212061
Megan McBride <megan.mcbride@colorado.edu>
2021-10-26 23:45:34 -0600

    Completing the full site fetch

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -44,10 +44,19 @@ self.addEventListener('fetch', e => {
     fetch(e.request)
       .then(res => {
         //Make clone of response
-        const resClone
-      })
-    )
-})
+        const resClone = res.clone();
+        //Open cache
+        caches
+          .open(cacheName)
+          .then(cache => {
+            //Add response to cache
+            cache.put(e.request, resClone);
+          });
+        return res;
+      }).catch(err => caches.match(e.request).then(res => res))
+      
+    );
+});
 
 
 //Previous fetch*****
