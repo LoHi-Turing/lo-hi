c13205f1f01e54f055e74d0e04fdac77c9fbf89d
Megan McBride <megan.mcbride@colorado.edu>
2021-10-26 23:42:24 -0600

    Now building out service worker to cache the entire site

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,24 +1,25 @@
 const self = this;
 
-const cacheName = 'v1';
-const cachedAssets = [
-  'index.html', 
-  'images/Lohi-computer-1.png',
-  '/src/index.css'
-]
+const cacheName = 'v2';
+// const cachedAssets = [
+//   'index.html', 
+//   'images/LoHi-computer-1.png',
+//   '/src/index.css',
+//   'images/Lohi-yellow.png'
+// ]
 
 self.addEventListener('install', e => {
   console.log('Service Worker: Installed');
   
-  e.waitUntil(
-    caches
-      .open(cacheName)
-      .then(cache => {
-        console.log('Service Worker: Caching Files')
-        cache.addAll(cachedAssets);
-      })
-      .then(() => self.skipWaiting())
-  );
+  // e.waitUntil(
+  //   caches
+  //     .open(cacheName)
+  //     .then(cache => {
+  //       console.log('Service Worker: Caching Files')
+  //       cache.addAll(cachedAssets);
+  //     })
+  //     .then(() => self.skipWaiting())
+  // );
 })
 
 self.addEventListener('activate', e => {
@@ -40,6 +41,19 @@ self.addEventListener('activate', e => {
 self.addEventListener('fetch', e => {
   console.log('Service Worker: fetching');
   e.respondWith(
-    fetch(e.request).catch()
+    fetch(e.request)
+      .then(res => {
+        //Make clone of response
+        const resClone
+      })
     )
 })
+
+
+//Previous fetch*****
+// self.addEventListener('fetch', e => {
+//   console.log('Service Worker: fetching');
+//   e.respondWith(
+//     fetch(e.request).catch(() => caches.match(e.request))
+//     )
+// })
